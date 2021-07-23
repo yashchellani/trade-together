7214ac0bfca2249b71e4054fe032a9dfae3569cd
yashchellani <yashcchellani@gmail.com>
2021-07-24 04:55:37 +0800

    sdksdnf

--- trade-together/src/App.jsx
+++ trade-together/src/App.jsx
@@ -1,8 +1,11 @@
    // src/App.js
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Contacts from './components/contacts';
 import Card from "./components/Card";
 import stonk from "./resources/assets/stonk.jpeg";
+import PrivatePage from './components/PrivatePage';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/LoginPage/LoginPage';
 
 
 const stock = {
@@ -11,33 +14,44 @@ const stock = {
   image: stonk,
   description:
     "This is a stock. Not chicken stock, not beef stock, not vegetable stalk. Just, stock."
-}
+};
 
-class App extends Component {
-  
+export default function App(){
 
-  
-  state = {
-    contacts: []
-  }
+  // state = {
+  //   co ntacts: []
+  // }
+  //render() {
+    const [token, setToken] = useState();
 
-  render() {
+    if(!token) {
+      return <Login setToken={setToken} />
+    }
     return (
       <div className="App">
-        <header className="App-header">
-          <Card
-            name={stock.name}
-            price={stock.price}
-            image = {stock.stonk}
-            description={stock.description}
-          />
-        </header>
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+              <Switch>
+                <Route path="/privatepage">
+                  <PrivatePage />
+                </Route>
+                <Route path="/">
+                  <Card
+                    name={stock.name}
+                    price={stock.price}
+                    image = {stock.stonk}
+                    description={stock.description}
+                  />
+                </Route>
+              </Switch>
+            </BrowserRouter>
+          </div>
       </div>
 
-      //<Contacts contacts={this.state.contacts} />
-
+      //<Contacts contacts={this.state.contacts} /
     );
-  }
+  //}
 
   //   componentDidMount() {
   //   fetch('http://jsonplaceholder.typicode.com/users')
@@ -48,5 +62,4 @@ class App extends Component {
   //   .catch(console.log)
   // }
 }
-
-export default App;
+//export default App;
